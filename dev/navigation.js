b95fd582c4f89d03ae99cf30b442d6104de179f1
MLTDK <maxime.aupetitalot@epita.fr>
2017-07-18 16:24:46 +0200

    0.0.5: normalize interface from Scala MM100

--- dev/navigation.js
+++ dev/navigation.js
@@ -1,3 +1,23 @@
 function gotoHref(target) {
 	location.href = target;
 }
+
+function paramsArray() {
+	return location.search.substr(1).split('&');
+}
+
+function getParam(param) {
+	var params = paramsArray();
+	var iprm, value = null;
+	
+	for (var i = 0; i < params.length; i++) {
+		iprm = params[i].split('=');
+		
+		if (param == iprm[0]) {
+			value = iprm[1];
+			break;
+		}
+	}
+	
+	return value;
+}
