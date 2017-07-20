d7027782744d76c66664828b244f58e2886e7438
MLTDK <maxime.aupetitalot@epita.fr>
2017-07-20 17:16:29 +0200

    0.0.9: first editor tests

---
+++ dev/editor.js
@@ -0,0 +1,23 @@
+function boldElem(elem) {
+	document.getElementById(elem).style="font-weight: bold;";
+}
+
+function italicElem(elem) {
+	document.getElementById(elem).style="font-style: italic;";
+}
+
+function underlineElem(elem) {
+	document.getElementById(elem).style="text-decoration: underline;";
+}
+
+function leftAlignElem(elem) {
+	document.getElementById(elem).style="text-align: left;";
+}
+
+function centerAlignElem(elem) {
+	document.getElementById(elem).style="text-align: center;";
+}
+
+function rightAlignElem(elem) {
+	document.getElementById(elem).style="text-align: right;";
+}
