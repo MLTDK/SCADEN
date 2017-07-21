2716f23f4e0f79ef5f6a167356f51a6eb966b254
MLTDK <maxime.aupetitalot@epita.fr>
2017-07-21 11:53:39 +0200

    0.0.95: Futura font

--- dev/editor.js
+++ dev/editor.js
@@ -1,31 +1,35 @@
-function boldElem(elem) {
-	document.getElementById(elem).style="font-weight: bold;";
-}
-
-function italicElem(elem) {
-	document.getElementById(elem).style="font-style: italic;";
-}
-
-function underlineElem(elem) {
-	document.getElementById(elem).style="text-decoration: underline;";
-}
-
-function leftAlignElem(elem) {
-	document.getElementById(elem).style="text-align: left;";
-}
-
-function centerAlignElem(elem) {
-	document.getElementById(elem).style="text-align: center;";
-}
-
-function rightAlignElem(elem) {
-	document.getElementById(elem).style="text-align: right;";
-}
-
 function shadowElem(elem) {
-	document.getElementById(elem).style="text-shadow: 1px 1px";
+	document.getElementById(elem).style = "text-shadow: 1px 1px";
 }
 
 function outlineElem(elem) {
-	document.getElementById(elem).style="-webkit-text-stroke: 4px navy;";
+	document.getElementById(elem).style = "-webkit-text-stroke: 4px navy;";
+}
+
+function fontElem(elem) {
+	document.getElementById(elem).style = "font-family: 'SFutura', serif; font-size: 24px;";
+}
+
+function boldElem(elem) {
+	document.getElementById(elem).style = "font-weight: bold;";
+}
+
+function italicElem(elem) {
+	document.getElementById(elem).style = "font-style: italic;";
+}
+
+function underlineElem(elem) {
+	document.getElementById(elem).style = "text-decoration: underline;";
+}
+
+function leftAlignElem(elem) {
+	document.getElementById(elem).style = "text-align: left;";
+}
+
+function centerAlignElem(elem) {
+	document.getElementById(elem).style = "text-align: center;";
+}
+
+function rightAlignElem(elem) {
+	document.getElementById(elem).style = "text-align: right;";
 }
