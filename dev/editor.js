907ba2a1b0b1424001422c396208b24710fd9879
MLTDK <maxime.aupetitalot@epita.fr>
2017-07-21 16:04:32 +0200

    0.0.98: drag & drop element

--- dev/editor.js
+++ dev/editor.js
@@ -33,3 +33,45 @@ function centerAlignElem(elem) {
 function rightAlignElem(elem) {
 	document.getElementById(elem).style = "text-align: right;";
 }
+
+function getElemOffset(elem) {
+	var left = 0,
+		top = 0;
+	
+	do {
+		left += elem.offsetLeft;
+		top += elem.offsetTop;
+	} while (elem = elem.offsetParent)
+	
+	return {
+		left: left,
+		top: top
+	}
+}
+
+function enableSelection() {
+	var elem1 = document.getElementById("elem1"),
+		elemStorage = {};
+	var pageEdit = document.getElementById("page_edit");
+	
+	elem1.addEventListener("mousedown", function(e) {
+		elemStorage.target = e.target;
+		elemStorage.offsetX = e.clientX - getElemOffset(elemStorage.target).left;
+		elemStorage.offsetY = e.clientY - getElemOffset(elemStorage.target).top;
+	});
+	
+	elem1.addEventListener("mouseup", function() {
+		elemStorage = {};
+	});
+	
+	pageEdit.addEventListener("mousemove", function(e) {
+		var target = elemStorage.target;
+		
+		if (target) {
+			target.style.left = e.clientX - elemStorage.offsetX + 'px';
+			target.style.top = e.clientY - elemStorage.offsetY + 'px';
+		}
+	});
+}
+
+enableSelection();
